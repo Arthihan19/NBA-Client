6ea48848bcffff28897d7acd772e5a01bbf0d609
Arthihan19 <arthihan19@gmail.com>
2024-01-03 14:37:46 +1100

    Added some boiler plate code

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { NavBar } from 'app/components/NavBar';
+import { PageWrapper } from 'app/components/PageWrapper';
 
 export function HomePage() {
   return (
     <>
       <Helmet>
-        <title>HomePage</title>
-        <meta name="description" content="A Boilerplate application homepage" />
+        <title>Home Page</title>
+        <meta
+          name="description"
+          content="A React Boilerplate application homepage"
+        />
       </Helmet>
-      <span>My HomePage</span>
+      <NavBar />
+      <PageWrapper></PageWrapper>
     </>
   );
 }
